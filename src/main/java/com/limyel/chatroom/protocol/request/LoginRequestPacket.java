c8d32ae141c47d7d526912e36e2a25d99a8e9695
limyel <limyel@outlook.com>
2022-12-06 17:14:27 +0800

    添加lombok注解

--- src/main/java/com/limyel/chatroom/protocol/request/LoginRequestPacket.java
+++ src/main/java/com/limyel/chatroom/protocol/request/LoginRequestPacket.java
@@ -2,7 +2,9 @@ package com.limyel.chatroom.protocol.request;
 
 import com.limyel.chatroom.constant.CommandConstant;
 import com.limyel.chatroom.protocol.AbstractPacket;
+import lombok.Data;
 
+@Data
 public class LoginRequestPacket extends AbstractPacket {
 
     private Integer userId;
