d1b8236afad92d05683b7c9eba377981906b47c6
limyel <limyel@outlook.com>
2022-12-07 14:50:20 +0800

    指令常量

--- src/main/java/com/limyel/chatroom/constant/CommandConstant.java
+++ src/main/java/com/limyel/chatroom/constant/CommandConstant.java
@@ -9,4 +9,35 @@ public class CommandConstant {
 
     public static final byte LOGIN_RESPONSE = 2;
 
+    public static final byte MESSAGE_REQUEST = 3;
+
+    public static final byte MESSAGE_RESPONSE = 4;
+
+    public static final byte LOGOUT_REQUEST = 5;
+
+    public static final byte LOGOUT_RESPONSE = 6;
+
+    public static final byte CREATE_GROUP_REQUEST = 7;
+
+    public static final byte CREATE_GROUP_RESPONSE = 8;
+
+    public static final byte LIST_GROUP_MEMBERS_REQUEST = 9;
+
+    public static final byte LIST_GROUP_MEMBERS_RESPONSE = 10;
+
+    public static final byte JOIN_GROUP_REQUEST = 11;
+
+    public static final byte JOIN_GROUP_RESPONSE = 12;
+
+    public static final byte QUIT_GROUP_REQUEST = 13;
+
+    public static final byte QUIT_GROUP_RESPONSE = 14;
+
+    public static final byte GROUP_MESSAGE_REQUEST = 15;
+
+    public static final byte GROUP_MESSAGE_RESPONSE = 16;
+
+    public static final byte HEARTBEAT_REQUEST = 17;
+
+    public static final byte HEARTBEAT_RESPONSE = 18;
 }
